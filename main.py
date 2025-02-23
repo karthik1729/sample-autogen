c958ca57539bc6180adb051a84290ef3346544e4
Karthik Th <karthik@kloudlite.io>
2025-02-23 10:43:06 +0530

    changed to remove docker dependency

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import os
-import autogen
 from autogen import AssistantAgent, UserProxyAgent
 
-llm_config = {"model": "gpt-4", "api_key": os.environ["OPENAI_API_KEY"]}
+llm_config = { "config_list": [{ "model": "gpt-4", "api_key": os.environ.get("OPENAI_API_KEY") }] }
+assistant = AssistantAgent("assistant", llm_config=llm_config)
+user_proxy = UserProxyAgent("user_proxy", code_execution_config=False)
 
-with autogen.coding.DockerCommandLineCodeExecutor(work_dir="coding") as code_executor:
-    assistant = AssistantAgent("assistant", llm_config=llm_config)
-    user_proxy = UserProxyAgent(
-        "user_proxy", code_execution_config={"executor": code_executor}
-    )
-
-    # Start the chat
-    user_proxy.initiate_chat(
-        assistant,
-        message="Plot a chart of NVDA and TESLA stock price change YTD. Save the plot to a file called plot.png",
-    )
+# Start the chat
+user_proxy.initiate_chat(
+    assistant,
+    message="Tell me a joke about NVDA and TESLA stock prices.",
+)
